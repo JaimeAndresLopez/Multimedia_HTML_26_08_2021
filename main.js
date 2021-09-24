b96f07f859c433ac019fe34c7d0819e5d8258819
JaimeAndresLopez <james_2005_scott@hotmail.com>
2021-09-24 16:00:11 -0500

    Multimedia I 26_08_2021

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log(`Hola Mundo`);
+// addEventListener("DOMContentLoaded", (e)=>{
+//    fetch("https://di-uploads-pod13.dealerinspire.com/mazdaofescondido/uploads/2019/10/Mazda-CX-9-Silver.png")
+//    .then(res=>res.blob())
+//    .then((res)=>{
+//        let img = document.createElement("IMG");
+//        img.src =URL.createObjectURL(res);
+//        document.body.appendChild(img);
+//    })
+// })
